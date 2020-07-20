7217aefe806889c963c626473a91658c6992e220
Elliot Parker <elliotccp@protonmail.com>
2020-07-20 13:57:20 +1000

    Update compiler

--- Lab2.java
+++ Lab2.java
@@ -1,48 +1,36 @@
-/**
- * @Elliot Parker
- * @2020-07-20
- */
-
 import java.util.*;
 
-public class Main
-{
-    public void Main()
-    {
-        Scanner scan = new Scanner(System.in);
+public class Lab2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int intLotteryNumber = (int) (Math.random() * (900) + 100);
         System.out.print("What number: ");
-        int x = scan.nextInt();
-        while(x < 100 || x > 999) 
-        {
+        int x = input.nextInt();
+        while (x < 100 || x > 999) {
             System.out.println("Try again");
             System.out.print("What number: ");
-            x = scan.nextInt();
+            x = input.nextInt();
         }
+        input.close();
         System.out.println("The number was " + intLotteryNumber);
-        if(x == intLotteryNumber) {
+        if (x == intLotteryNumber) {
             System.out.println("You have won $10000");
-        } else 
-        {   
+        } else {
             boolean wonDigit = false;
             int digitsUser = x;
-            while(digitsUser > 0 && !wonDigit) {
+            while (digitsUser > 0 && !wonDigit) {
                 int digitsLott = intLotteryNumber;
-                while(digitsLott > 0 && !wonDigit) 
-                {
-                    if(digitsUser % 10 == digitsLott % 10)
-                    {
+                while (digitsLott > 0 && !wonDigit) {
+                    if (digitsUser % 10 == digitsLott % 10) {
                         wonDigit = true;
                     }
                     digitsLott = digitsLott / 10;
                 }
                 digitsUser = digitsUser / 10;
             }
-            if(wonDigit)
-            {
+            if (wonDigit) {
                 System.out.println("You have won $1000");
-            } else 
-            {
+            } else {
                 System.out.println("You have lost");
             }
         }
